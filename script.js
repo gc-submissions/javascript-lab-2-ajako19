98967e50d824d24cfc18b068ae78a2816dba4d1f
ajako19 <97720704+ajako19@users.noreply.github.com>
2022-02-23 18:24:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// TODO - write your code here.
+
+const randomDamage = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+// console.log(randomDamage());
+
+const chooseOption = (opt1, opt2) => {
+    let randNum =  Math.floor(Math.random() * 2);
+    return randNum === 0 ? opt1 : opt2;
+}
+
+// console.log(chooseOption());
+// make sure this is right too later
+
+const attackPlayer = function(health) {
+    return health =  health - randomDamage();
+}
+
+// console.log(attackPlayer());
+
+const logHealth = (player, health) => {
+    console.log(`${player} health: ${health}`)
+}
+
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}`)
+}
+
+const isDead = (health) => {
+    // return (health <= 0
+    if (health <= 0) {return true} else {return false}
+    // top and bottom returns work the same
+}
+
+function fight(player1, player2, player1Health, player2Health) {
+    while (true) {
+        let attacker = chooseOption(player1, player2);
+        if (attacker === player1) {
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if (isDead(player2Health) === true) {
+                logDeath(player1, player2);
+                break;
+            }
+        } else {
+            player1Health = attackPlayer(player1Health);
+            logHealth(player1, player1Health);
+            if (isDead(player1Health) === true)  {
+                logDeath(player2, player1);
+                break;
+            }
+        }
+    }
+}
+
+fight("Jakob", "Michael", 100, 100);
